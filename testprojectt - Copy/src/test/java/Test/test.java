e6ce33120128ea1ffe1cbc2f31c5e031a53f8342
SatpatiAnanya <ananya_satpati@rediffmail.com>
2025-02-28 14:18:05 +0530

    6th commit

---
+++ testprojectt - Copy/src/test/java/Test/test.java
@@ -0,0 +1,38 @@
+package Test;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+
+import Helper.BaseTest;
+import testprojectt.testprojectt.App;
+
+public class test extends BaseTest {
+    
+   
+    @Test(priority = 1)
+    public void openGooglePage() {
+        driver.get("https://www.google.com/");
+        System.out.println("Page's new title is: " + driver.getTitle());
+    }
+    
+    @Test(priority = 2, retryAnalyzer = Helper.RetryAnalyzer.class)
+	public void Search_feature() {
+    	openGooglePage();	
+    	App aut = new App(driver);
+		Helper.Interactions interact = new Helper.Interactions(driver);
+		interact.executeWithDelay(() -> aut.searchValidation());
+	}
+
+    @Test(priority = 3, retryAnalyzer = Helper.RetryAnalyzer.class)
+	public void Validate_second_JOIN_US_button_click_TC_003() {
+    	openGooglePage();	
+    	App aut = new App(driver);
+		Helper.Interactions interact = new Helper.Interactions(driver);
+		interact.executeWithDelay(() -> aut.clickFeelingluckyBtn());
+	}
+	 @AfterMethod
+		public void afterMethod() throws InterruptedException {
+			Thread.sleep(2000);
+			driver.quit();
+	 }    
+}
